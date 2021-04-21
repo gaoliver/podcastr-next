76f81bf4faace8a54d56a21d68d351528690331f
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-04-20 23:48:34 -0300

    Dia 2 - SSG (Static Site Generator)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
 export default function Home(props) {
-  console.log(props.episodes)
   return (
-    <h1>Index</h1>
+    <>
+      <h1>Página principal (em desenvolvimento)</h1>
+      <br />
+      <ul>
+        {props.episodes.map((ep) => (
+          <li key={ep.id} style={{ marginTop: '15px' }}>{ep.title}</li>
+        ))}
+      </ul>
+    </>
+
   )
 }
 
-export async function getServerSideProps() {
+// export async function getServerSideProps() {   /// Server Side Render (SSR)
+export async function getStaticProps() {          /// Static Site Generator
   const response = await fetch('http://localhost:3001/episodes')
   const data = await response.json()
 
   return {
     props: {
       episodes: data
-    }
+    },
+    /* For SSG - in seconds. The request will be done each 8 hours. */
+    revalidate: 60 * 60 * 8
   }
 }
